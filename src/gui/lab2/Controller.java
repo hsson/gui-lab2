59d121313d765b53dccfd84920092d7131eb06a4
Alexander Håkansson <al3xand3r.h@gmail.com>
2015-01-27 17:06:21 +0100

    Implements listener methods with dummy values

--- src/gui/lab2/Controller.java
+++ src/gui/lab2/Controller.java
@@ -1,4 +1,66 @@
 package gui.lab2;
 
-public class Controller {
+import com.sun.javafx.collections.ObservableListWrapper;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    public ComboBox<String> comboCuisine;
+    public ComboBox<String> comboIngredient;
+    public TextField textMaxPrice;
+    public Slider sliderMaxTime;
+    public ToggleButton toggleEasy;
+    public ToggleButton toggleMedium;
+    public ToggleButton toggleHard;
+
+    private Model model = new Model();
+    private int oldSliderVal;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        comboCuisine.setItems(new ObservableListWrapper<String>(model.getCuisineList()));
+        comboIngredient.setItems(new ObservableListWrapper<String>(model.getIngredientList()));
+
+        sliderMaxTime.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov,
+                                Number old_val, Number new_val) {
+                int val = new_val.intValue();
+                if (val % 10 == 0 && val != oldSliderVal) {
+                    sliderMaxTimeOnSelected(val);
+                    oldSliderVal = val;
+                }
+            }
+        });
+    }
+
+    public void comboCuisineOnAction() {
+        System.out.println(comboCuisine.getValue());
+    }
+
+    public void comboIngredientOnAction() {
+        System.out.println(comboIngredient.getValue());
+    }
+
+    public void textMaxPriceOnAction() {
+        System.out.println(textMaxPrice.getText());
+    }
+
+    public void toggleDiffOnAction(ActionEvent event) {
+        ToggleButton tb = (ToggleButton) event.getSource();
+        System.out.println(tb.getText());
+    }
+
+    public void sliderMaxTimeOnSelected(int value) {
+        System.out.println(value);
+    }
 }
---
+++ src/gui/lab2/Model.java
@@ -0,0 +1,38 @@
+package gui.lab2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Model {
+    private List<String> cuisineList = new ArrayList<String>();
+    private List<String> ingredientList = new ArrayList<String>();
+
+    public Model() {
+        initCuisineList();
+        initIngredientList();
+    }
+
+    private void initCuisineList() {
+        cuisineList.add("Sverige");
+        cuisineList.add("Grekland");
+        cuisineList.add("Indien");
+        cuisineList.add("Asien");
+        cuisineList.add("Afrika");
+        cuisineList.add("Frankrike");
+    }
+
+    private void initIngredientList() {
+        ingredientList.add("Kött");
+        ingredientList.add("Fisk");
+        ingredientList.add("Kyckling");
+        ingredientList.add("Vegetarisk");
+    }
+
+    public List<String> getCuisineList() {
+        return new ArrayList<String>(cuisineList);
+    }
+
+    public List<String> getIngredientList() {
+        return new ArrayList<String>(ingredientList);
+    }
+}
