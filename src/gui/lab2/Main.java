722d578e91e76d83b227a283629ccc9eacf7bbaa
Alexander Håkansson <al3xand3r.h@gmail.com>
2015-01-27 16:03:15 +0100

    Adds all search elements

--- src/gui/lab2/Main.java
+++ src/gui/lab2/Main.java
@@ -12,7 +12,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
         primaryStage.setTitle("GUI Lab 2");
-        primaryStage.setScene(new Scene(root));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/res/style/customStyle.css");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
